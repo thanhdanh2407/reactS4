ba79c25f29711d7e1b79208901503af2550ac775
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-03-19 09:59:44 +0700

    The first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 19Mar
+<h1>heheehhee</h1>
---
+++ app.js
@@ -0,0 +1,45 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//localhost:3000/
+app.use('/', require('./routes/index'));
+//localhost:3000/users
+app.use('/users', require('./routes/users'));
+//localhost:3000/books
+app.use('/books', require('./routes/books'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    success:false,
+    message: err.message
+  });
+});
+
+module.exports = app;
